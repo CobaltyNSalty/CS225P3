5182300f4d16e0089242470164920c5dae69354c
CobaltyNSalty <96799127+CobaltyNSalty@users.noreply.github.com>
2023-03-11 18:57:02 -0500

    Added work log comment to each class

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,11 @@
+/* Work Log: add your name in brackets, the date, and a brief summary of what you contributed that day.
+The assignment details that code you wrote requires a comment with your name above it. We will implement
+that using this process. Log your work up here, and if you make a revision to existing code then add your
+name in a comment on the same line to not interfere with other important documentation requirements.
+
+3/11    [chris]     - Created class, added work log comment.
+
+
+ */
 public class Car {
 }
--- src/Launcher.java
+++ src/Launcher.java
@@ -1,3 +1,12 @@
+/* Work Log: add your name in brackets, the date, and a brief summary of what you contributed that day.
+The assignment details that code you wrote requires a comment with your name above it. We will implement
+that using this process: Log your work up here, and if you make a revision to existing code then add your
+name in a comment on the same line to not interfere with other important documentation requirements.
+
+3/11    [chris]     - Created class, added work log comment.
+
+
+ */
 public class Launcher {
     public static void main(String[] args) {
 
