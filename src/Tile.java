8bee152337ec55da94d62e39fa1a0da4938d924c
CobaltyNSalty <96799127+CobaltyNSalty@users.noreply.github.com>
2023-03-21 11:56:00 -0400

    fixed test

--- src/Tile.java
+++ src/Tile.java
@@ -16,9 +16,9 @@ name in a comment on the same line to not interfere with other important documen
 public class Tile extends JLabel {
     /* ___ FIELD VARIABLES ___ */
     /* Display image */
-    // private Image sprite;
+    //private Image sprite;
     /* Sequence of points from entry point to exit point that Car objects travel across this Tile */
-    //private Point[] path;
+    private Point[] path;
     /* A value to represent the specific type of Tile, correlates to TrackTile filename of image */
     private int tileIDNum;
     /* The row index of this Tile within the array housing it
