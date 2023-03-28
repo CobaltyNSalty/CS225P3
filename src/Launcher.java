aafb6624bda5d3423c897daffdf544def3c171b3
escape9179 <tr3.logan@gmail.com>
2023-03-27 20:27:36 -0400

    add windows 95 buttons LAF

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 /* Work Log: add your name in brackets, the date, and a brief summary of what you contributed that day.
 The assignment details that code you wrote requires a comment with your name above it. We will implement
 that using this process: Log your work up here, and if you make a revision to existing code then add your
@@ -10,6 +12,17 @@ name in a comment on the same line to not interfere with other important documen
  */
 public class Launcher {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
         Game game = new Game();
         game.play();
     }
